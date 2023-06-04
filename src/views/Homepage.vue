33489095709f215595e4cfdfdb10527e83f0db0c
Dario Di Nicuolo <dado02rio02@gmail.com>
2023-06-04 11:49:02 +0200

    Assemblata homepage con product list

---
+++ src/views/Homepage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Homepage</h1>
+    <ProductList />
+  </div>
+</template>
+
+<script>
+import ProductList from '../components/ProductList';
+
+export default {
+  components: {
+    ProductList,
+  },
+};
+</script>
+
+<style scoped>
+</style>
