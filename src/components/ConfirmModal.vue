686fb1490f5241b1e3e05718fcb5f12712bf6f7a
Dario Di Nicuolo <dado02rio02@gmail.com>
2023-06-06 11:46:22 +0200

    Creata modale di conferma eliminazione di un prodotto all'interno del carrello

---
+++ src/components/ConfirmModal.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="confirmModalLabel">Delete {{ product.name }}?</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          Are you sure you want to delete {{ product.name }} from your cart?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+          <button type="button" class="btn btn-danger" @click="removeProduct(product)">Delete</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      product: {
+        type: Object,
+        required: true,
+      },
+    },
+    methods: {
+      removeProduct(product) {
+        $('#confirmModal').modal('toggle');
+        this.$emit('deletion-confirmed', product);
+      },
+    },
+  };
+</script>
